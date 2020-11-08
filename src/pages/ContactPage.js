65c571d4b6d79def9bcfdb93a67fc24bbfa46752
fabianzielinski <fabian.zielinski@o2.pl>
2020-11-08 20:29:07 +0100

    Add ContactPage textarea

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,36 @@
-const ContactPage = () => {
-    return ( <div>Kontakt</div> );
+import React, { Component } from 'react';
+import {Prompt} from 'react-router-dom';
+import '../styles/ContactPage.css'
+
+class ContactPage extends Component {
+    state = { 
+        value: "",
+     }
+
+     handleSubmit = (e) => {
+         e.preventDefault()
+         this.setState({
+             value: "",
+         })
+     }
+ 
+     handleChange = (e) => {
+         this.setState({
+             value: e.target.value,
+         })
+     }
+
+    render() { 
+        return (
+            <div className="contact">
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Napisz do nas</h3>
+                    <textarea value={this.state.value} onChange={this.handleChange} placeholder="Wpisz wiadomość ..."></textarea>
+                    <button>Wyślij</button>
+                </form>
+            </div>
+          );
+    }
 }
  
 export default ContactPage;
